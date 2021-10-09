db516d117d55b00ccfb3903767332d5f4edc0e8c
Hardik <hardik8784@gmail.com>
2021-10-09 16:47:01 -0400

    Created Majorly AccountLogin System

--- Assets/Scripts/GameSystemManager.cs
+++ Assets/Scripts/GameSystemManager.cs
@@ -8,7 +8,7 @@ public class GameSystemManager : MonoBehaviour
 
     GameObject InputFieldUserName, InputFieldPassword, SubmitButton, ToggleLogin, ToggleCreate;
 
-
+    GameObject NetworkedClient;
 
 
 
@@ -28,6 +28,8 @@ public class GameSystemManager : MonoBehaviour
                 ToggleLogin = go;
             else if (go.name == "ToggleCreate")
                 ToggleCreate = go;
+            else if (go.name == "NetworkedClient")
+                NetworkedClient = go;
         }
 
         SubmitButton.GetComponent<Button>().onClick.AddListener(SubmitButtonPressed);
@@ -38,12 +40,27 @@ public class GameSystemManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SubmitButtonPressed()
     {
-        Debug.Log("We Good..Submit Button Pressed..!!");
+        //Debug.Log("We Good..Submit Button Pressed..!!");
+
+        string n = InputFieldUserName.GetComponent<InputField>().text;
+        string p = InputFieldPassword.GetComponent<InputField>().text;
+
+        if (ToggleLogin.GetComponent<Toggle>().isOn)
+        {
+            NetworkedClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToServerSignifiers.Login + "," + n + "," + p);
+            //Debug.Log(ClientToServerSignifiers.Login + "," + n + "," + p);
+        }
+        else
+        {
+            NetworkedClient.GetComponent<NetworkedClient>().SendMessageToHost(ClientToServerSignifiers.CreateAccount + "," + n + "," + p);
+            //Debug.Log(ClientToServerSignifiers.CreateAccount + "," + n + "," + p);
+        }
+
     }
 
     public void ToggleCreateValueChanged(bool newValue)
@@ -55,4 +72,32 @@ public class GameSystemManager : MonoBehaviour
     {
         ToggleCreate.GetComponent<Toggle>().SetIsOnWithoutNotify(!newValue);
     }
+}
+    public static class ClientToServerSignifiers
+    {
+        public const int Login = 1;
+
+        public const int CreateAccount = 2;
+    }
+
+    public static class ServerToClientSignifiers
+    {
+        public const int LoginResponse = 1;
+
+        //public const int LoginFailure = 2;
+
+        //public const int CreateAccountSuccess = 1;
+
+        //public const int CreateAccountFailure = 2;
+    }
+
+public static class LoginResponses
+{
+    public const int Success = 1;
+
+    public const int FailureNameInUse = 2;
+
+    public const int FailureNameNotFound = 3;
+
+    public const int IncorrectPassword = 4;
 }
